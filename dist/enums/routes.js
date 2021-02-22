49c17f0e47bbd690cf94dcfd817eaa827c639296
Henry Liu <jiah.liu@mail.utoronto.ca>
2021-02-22 01:36:24 -0500

    remove dist folder

--- dist/enums/routes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VALIDATE_RIDE_ROUTE = void 0;
-const VALIDATE_RIDE_ROUTE = '/check-in';
-exports.VALIDATE_RIDE_ROUTE = VALIDATE_RIDE_ROUTE;
-//# sourceMappingURL=routes.js.map
