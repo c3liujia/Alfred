497b7114ded84b0e710a86455eda834be0c285db
Henry Liu <jiah.liu@mail.utoronto.ca>
2021-02-21 20:56:56 -0500

    Rename AWS env

--- src/helpers/sendEmail.ts
+++ src/helpers/sendEmail.ts
@@ -1,7 +1,7 @@
 
 var AWS = require('aws-sdk');
 // you shouldn't hardcode your keys in production! See http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-configuring.html
-AWS.config.update({accessKeyId: process.env.accessKeyId, secretAccessKey: process.env.secretAccessKey});
+AWS.config.update({accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY});
 
 export function sendEmail(userId:string): void {
   var lambda = new AWS.Lambda();
